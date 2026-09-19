a59dd5985c105e05858bb7ddc1c53dfd6bc8d6cf
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-09-19 23:37:49 +0800

    fix: add missing sqflite package declaration

--- lib/screens/dashboard/add_device_screen.dart
+++ lib/screens/dashboard/add_device_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../theme/app_colors.dart';
 import '../../theme/app_theme.dart';
